9ece340590496d17af0cf8c51be37506b0fdded3
Nav73 <nhued1@gmail.com>
2024-12-27 16:19:22 +0530

    final working on vercel

--- index.js
+++ index.js
@@ -1,66 +1,58 @@
-//jshint esversion:6
-
+// jshint esversion: 6
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 const path = require("path");
-const { title } = require("process");
-require("dotenv").config()
+require("dotenv").config();
 
 const app = express();
+const PORT = process.env.PORT || 3004;
+const uri = process.env.MONGO_URI;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-const PORT = process.env.PORT || 3000;
-const uri = process.env.MONGODB_URI
 
-mongoose
-  .connect(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+// MongoDB connect
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-//blog Schema
-const blogSchema = new mongoose.Schema({
-  title: { type: String, require: true },
-  post: { type: String, require: true },
-});
-//create Schema
-const Blog = new mongoose.model("Blog", blogSchema);
+// Blog Schema
+const blogSchema = {
+  title: { type: String, required: true },
+  post: { type: String, required: true },
+};
 
+// Create Blog model
+const Blog = mongoose.model("Blog", blogSchema);
+
+// Static content for pages
 const homeStartingContent =
-  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+  "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo...";
 const aboutContent =
-  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+  "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper...";
 const contactContent =
-  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+  "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum...";
 
+// Routes
 
-
-let posts = [];
-
-app.get("/", function (req, res) {
-  //fetch all data from the database
-  const fetchAllBlogs = async () => {
-    try {
-      posts = await Blog.find();
-      // console.log(posts)
-    } catch (err) {
-      console.log("got error fetching all blogs: ", err);
-    }
-  };
-  fetchAllBlogs();
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts,
-  });
+// Home 
+app.get("/", async function (req, res) {
+  try {
+    const posts = await Blog.find();
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+    });
+  } catch (err) {
+    console.error("Error fetching blogs from MongoDB:", err);
+    res.status(500).send("Error fetching blogs.");
+  }
 });
 
 app.get("/about", function (req, res) {
@@ -75,36 +67,32 @@ app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose", function (req, res) {
-  // console.log("title: "+req.body.postTitle+" content: " + req.body.postBody)
-  //   const title = _.lowerCase(req.body.postTitle)
-  //   const content = _.lowerCase(req.body.postBody)
-  const title = req.body.postTitle.toLowerCase().replace(/\s+/g, "");
-  const content = req.body.postBody.toLowerCase().replace(/\s+/g, "");
-  console.log("title: " + title + " content: " + content);
-  const saveBlog = async () => {
-    const blog = new Blog({
-      title: title,
-      post: content,
-    });
+app.post("/compose", async function (req, res) {
+  const title = _.startCase(req.body.postTitle);
+  const content = req.body.postBody; 
+  // create new blog post
+  const newBlog = new Blog({
+    title: title,
+    post: content,
+  });
 
-    try {
-      const result = await blog.save();
-      console.log("Note Saved: ", result);
-    } catch (error) {
-      console.log("Error while saving the note: ", err);
-    }
-    // posts.push(post);
-  };
-  saveBlog();
-  res.redirect("/");
+  try {
+    // save blog to database
+    const result = await newBlog.save();
+    console.log("Note Saved: ", result);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error while saving the note:", err);
+    res.status(500).send("Error saving the blog post.");
+  }
 });
+
 
 app.get("/posts/:postId", function (req, res) {
   const requestedId = req.params.postId;
   const getPostById = async (postId) => {
     try {
-      const post = await Blog.findById(postId); // Fetch by ID
+      const post = await Blog.findById(postId);
       if (post) {
         console.log("Post found:", post);
         res.render("post", { title: post.title, content: post.post });
@@ -116,11 +104,12 @@ app.get("/posts/:postId", function (req, res) {
     }
   };
   getPostById(requestedId);
-  // res.send("hello")
 });
 
-// app.listen(3000, function () {
-//   console.log("Server started on port 3000");
-// });
 
-module.exports = app;                                                                 
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });    
+
+
+module.exports = app;                                                       
